b50321b17d0fbd60df21643d2249b55de6972322
Jelle Henkens <jelle.henkens@gmail.com>
2014-12-10 12:14:36 -0800

    Adding related stream endpoints to Distribution

--- src/Distribution.php
+++ src/Distribution.php
@@ -29,4 +29,35 @@ class Distribution extends Resource {
   	return $this->id;
   }
 
+/**
+ * Retrieve list of data streams associated with the distribution
+ *
+ * @return StreamCollection
+ */
+  public function streams() {
+    return new StreamCollection($this->client, $this);
+  }
+
+/**
+ * Get details of a specific data Stream associated with the
+ * distribution.
+ *
+ * @param string $name
+ * @return Stream
+ */
+  public function stream($name) {
+    return Stream::getStream($this->client, $this, $name);
+  }
+
+/**
+ * Update a data stream associated with the Distribution, if a
+ * stream with this name does not exist it gets created.
+ *
+ * @param string $name
+ * @param array $data
+ * @return Stream
+ */
+  public function updateStream($name, $data = array()) {
+    return Stream::createStream($this->client, $this, $name, $data);
+  }
 }
